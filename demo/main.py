bac735eaf8f2f61523ebb4366fd3456c9962bd2f
Vagner Bom Jesus <48354097+VagnerBomJesus@users.noreply.github.com>
2024-04-22 10:07:31 +0000

    create username and new file for user (bug to resolve)

--- demo/main.py
+++ demo/main.py
@@ -15,6 +15,11 @@ str_obj_status = file.read_from_file(filename)
 
 obj_status = json.loads(str_obj_status)
 
+input_text = ""  # This variable will store the current input value
+
+new_input_text = "" # This variable will store the
+
+
 @dataclass
 class ToDo:
     title: str
@@ -23,18 +28,41 @@ class ToDo:
 def handle_drop(todo: ToDo, location: str):
     ui.notify(f'"{todo.title}" is now in {location}')
 
+def on_input_change(event):
+    global input_text
+    input_text = event.value
+    print("USERNAME", input_text)
 
-for row_index in obj_status:
-    with ui.row():
-        for status  in row_index:
-            with dnd.column(status["col"], on_drop=handle_drop):
-                todos = status["todos"]
-                if len(todos) > 0:
-                    for todo in todos:
-                        dnd.card(ToDo(todo))
-                else:
-                    print(f'No todos in {status["col"]}')
-    
+
+
+def on_submit():
+    if input_text:
+        str_obj_status = file.read_from_file(filename)
+        new_input_text = f"new_{input_text}.json" 
+        file.write_to_file(new_input_text, str_obj_status)
+
+        obj_status = json.loads(str_obj_status)
+
+        for row_index in obj_status:
+            with ui.row():
+                for status  in row_index:
+                    with dnd.column(status["col"], on_drop=handle_drop):
+                        todos = status["todos"]
+                        if len(todos) > 0:
+                            for todo in todos:
+                                dnd.card(ToDo(todo))
+                        else:
+                            print(f'No todos in {status["col"]}')
+            
+
+
+
+
+
+
+
+
+
 
 
 
@@ -68,4 +96,11 @@ with ui.row():
     with dnd.column('Action', on_drop=handle_drop):
         print()
 '''
+
+username_input = ui.input(
+    placeholder='Enter username', 
+    on_change=on_input_change)
+
+submit_button = ui.button('Submit', on_click=on_submit)
+
 ui.run()
